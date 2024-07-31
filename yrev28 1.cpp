e91fefba3f62bb94cecfa01813678b41108fe559
yrev28 <ikhaishgi08@gmail.com>
2024-07-31 16:59:02 +0500

    Add files via upload

---
+++ yrev28 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+bool x;
+bool y;
+int oper;
+int main() {
+    cout<<"enter first number";
+    cin>>x;
+    cout<<"enter second number;
+    cin>>y;
+  return 0;
+}
